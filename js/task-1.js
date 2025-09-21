e1f7a5ad77cb593283f0e15e24cbee06f6cc5d60
Kira Yukhno <kira.yukhno123@gmail.com>
2025-09-21 15:34:19 +0300

    do tasks one through five, change css, add fonts

---
+++ js/task-1.js
@@ -0,0 +1,5 @@
+console.log(`Number of categories: ${categories.querySelectorAll("li.item").length}`);
+categories.querySelectorAll("li.item").forEach(category => {
+    console.log(`Category: ${category.querySelector("h2").textContent}`);
+    console.log(`Elements: ${category.querySelectorAll("li").length}`);
+});
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+    if (email === "" || password === "") {
+        return alert("All form fields must be filled in");
+    }
+    const user = {
+        email: email,
+        password: password,
+    };
+    console.log(user);
+    loginForm.reset();
+});
